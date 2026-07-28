c38bb362926dc41ad5c3f02304241cfffa2f15fa
rachit shrestha <rachitshrestha0@gmail.com>
2026-07-28 22:07:25 +0545

    UI for login page

--- music/accounts/views.py
+++ music/accounts/views.py
@@ -5,4 +5,7 @@ class UserLoginView(LoginView):
     template_name = "accounts/login.html"
     redirect_authenticated_user = True
 
+def register(request):
+    return render(request, "accounts/register.html")
+
 # Create your views here.
--- music/mysite/urls.py
+++ music/mysite/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('admin/', admin.site.urls),
      path("accounts/", include("accounts.urls")),
+     path('', LoginView.as_view(template_name='accounts/login.html'), name='home'),
 ]
