5a1864db96be4aa278c8d6b7ef5b22574c8c7781
Uami-11 <nirwan.m.9618@gmail.com>
2026-07-28 11:41:14 +0545

    fix: clean up duplicate imports in urls.py and register all music models in admin

--- music/music/admin.py
+++ music/music/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Album, Artist, Genre, ListeningHistory, Rating, Song, SongArtist
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "country")
+    search_fields = ("name",)
+    list_filter = ("country",)
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "artist", "release_year")
+    search_fields = ("name",)
+    list_filter = ("release_year",)
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "album", "genre", "duration")
+    search_fields = ("title",)
+    list_filter = ("genre",)
+
+
+@admin.register(SongArtist)
+class SongArtistAdmin(admin.ModelAdmin):
+    list_display = ("id", "song", "artist", "role")
+    list_filter = ("role",)
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "song", "rating")
+    list_filter = ("rating",)
+
+
+@admin.register(ListeningHistory)
+class ListeningHistoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "song", "played_at")
+    list_filter = ("played_at",)
