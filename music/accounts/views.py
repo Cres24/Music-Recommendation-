ea0fd6c1e61877ce13b68e949652742e096ecce4
rachit shrestha <rachitshrestha0@gmail.com>
2026-07-29 11:01:58 +0545

    registration

--- music/accounts/views.py
+++ music/accounts/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 
 class UserLoginView(LoginView):
     template_name = "accounts/login.html"
     redirect_authenticated_user = True
 
-def register(request):
-    return render(request, "accounts/register.html")
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created successfully!")
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "accounts/register.html", {"form": form})
