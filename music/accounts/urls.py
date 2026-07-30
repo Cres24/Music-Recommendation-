4be26782de0cb2edf325f3b5cfe6c89f397abcd1
Uami-11 <nirwan.m.9618@gmail.com>
2026-07-30 22:28:58 +0545

    fix: remove duplicate login path and clean up imports in accounts/urls.py

--- music/accounts/urls.py
+++ music/accounts/urls.py
@@ -1,16 +1,9 @@
 from django.urls import path
-from .views import UserLoginView
 from django.contrib.auth.views import LogoutView
-from .views import register
-from django.contrib.auth.views import LoginView
+from .views import UserLoginView, register
 
 urlpatterns = [
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
-    path(
-        "login/",
-        LoginView.as_view(template_name="accounts/login.html"),
-        name="login",
-    ),
     path("register/", register, name="register"),
 ]
