fd123dc736f473221773f866aeafe742ccc37910
Uami-11 <nirwan.m.9618@gmail.com>
2026-07-30 22:29:07 +0545

    feat: add UserRegistrationForm with email and first_name fields

--- music/accounts/forms.py
+++ music/accounts/forms.py
@@ -1,5 +1,8 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+from .models import User
+
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(
@@ -15,3 +18,45 @@ class LoginForm(AuthenticationForm):
             "placeholder": "Password"
         })
     )
+
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(
+        max_length=30,
+        required=False,
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Full Name"
+        })
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            "class": "form-control",
+            "placeholder": "Email Address"
+        })
+    )
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Username"
+        })
+    )
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={
+            "class": "form-control",
+            "placeholder": "Password"
+        })
+    )
+    password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput(attrs={
+            "class": "form-control",
+            "placeholder": "Confirm Password"
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "first_name", "password1", "password2"]
