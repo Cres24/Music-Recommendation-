7d4f4a38a0d3097bbb3e4fcfc4974c4ee6c1fb15
rachit shrestha <rachitshrestha0@gmail.com>
2026-08-09 22:21:55 +0545

    redirect to home.html and made base.html

--- music/music/urls.py
+++ music/music/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
 ]
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path("home/", views.home, name="home"),
+# ]
--- music/mysite/settings.py
+++ music/mysite/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -81,6 +81,20 @@ TEMPLATES = [
         },
     },
 ]
+# TEMPLATES = [
+#     {
+#         "BACKEND": "django.template.backends.django.DjangoTemplates",
+#         "DIRS": [],
+#         "APP_DIRS": True,
+#         "OPTIONS": {
+#             "context_processors": [
+#                 "django.template.context_processors.request",
+#                 "django.contrib.auth.context_processors.auth",
+#                 "django.contrib.messages.context_processors.messages",
+#             ],
+#         },
+#     },
+# ]
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
@@ -154,9 +168,13 @@ STORAGES = {
     },
 }
 
-LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "login"
-LOGIN_URL = "login"
+# LOGIN_REDIRECT_URL = "home"
+# LOGOUT_REDIRECT_URL = "login"
+# LOGIN_URL = "login"
+
+LOGIN_REDIRECT_URL = "/home/"
+LOGIN_URL = "/accounts/login/"
+LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 AUTH_USER_MODEL = "accounts.User"
 
