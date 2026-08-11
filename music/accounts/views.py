ca76beae63c3ab3aae7eaa923a6302efd2ee4ac5
rachit shrestha <rachitshrestha0@gmail.com>
2026-08-11 21:46:37 +0545

    removing repeated codes

--- music/accounts/views.py
+++ music/accounts/views.py
@@ -1,35 +1,9 @@
-# from django.shortcuts import render, redirect
-# from django.contrib.auth.views import LoginView
-# from django.contrib import messages
-
-# from .forms import LoginForm, UserRegistrationForm
-
-
-# class UserLoginView(LoginView):
-#     template_name = "accounts/login.html"
-#     redirect_authenticated_user = True
-#     authentication_form = LoginForm
-
-
-# def register(request):
-#     if request.method == "POST":
-#         form = UserRegistrationForm(request.POST)
-
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, "Account created successfully!")
-#             return redirect("login")
-#     else:
-#         form = UserRegistrationForm()
-
-#     return render(request, "accounts/register.html", {"form": form})
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm
 
-from .forms import RegisterForm
 
 
 def register(request):
