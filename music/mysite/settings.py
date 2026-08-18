20d8e5370822689eb4bdff8d37b35bba99c36097
rachit shrestha <rachitshrestha0@gmail.com>
2026-08-18 23:18:34 +0545

    remving unrequired code from settings.py

--- music/mysite/settings.py
+++ music/mysite/settings.py
@@ -81,20 +81,6 @@ TEMPLATES = [
         },
     },
 ]
-# TEMPLATES = [
-#     {
-#         "BACKEND": "django.template.backends.django.DjangoTemplates",
-#         "DIRS": [],
-#         "APP_DIRS": True,
-#         "OPTIONS": {
-#             "context_processors": [
-#                 "django.template.context_processors.request",
-#                 "django.contrib.auth.context_processors.auth",
-#                 "django.contrib.messages.context_processors.messages",
-#             ],
-#         },
-#     },
-# ]
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
